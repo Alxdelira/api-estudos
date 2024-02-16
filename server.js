40a43d66dfc49de3a47e857f4534d6ee3548930a
Alexandre <alx.delira@gmail.com>
2024-02-16 17:14:40 -0400

    vercel configuração 8

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-import app from "./src/app.js"
-import * as dotenv from "dotenv"
+import app from "./src/app.js";
+import * as dotenv from "dotenv";
 import swaggerUI from 'swagger-ui-express'; 
 import swaggerJsDoc from 'swagger-jsdoc';  
 import swaggerOptions from './src/docs/head.js'; 
 
-
-dotenv.config()
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 
+// Defina o URL do CSS do Swagger UI
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
+// Defina as opções do Swagger
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
+// Configure o Swagger UI Express
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs, { customCssUrl: CSS_URL }));
 
+// Inicie o servidor
 app.listen(port, () => console.log(`Servidor Rodando em http://localhost:${port}`));
