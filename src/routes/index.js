e178d44fa846df25156b32f138af32a6a306534c
Alexandre Nogueira de Lira <102405026+Alxdelira@users.noreply.github.com>
2024-02-22 18:37:48 -0400

    criando rota de visualizar imagens

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import usuarios from "./usuarioRouter.js"
 import login from "./loginRouter.js"
 import uploads from "./imageRouter.js"
+import express from "express"
 
 
 const routes = (app) => {
     app.route("/").get((req, res) => {
         res.status(200).redirect("/docs")
     });
+    app.use("/img", express.static('uploads'))
 
     app.use(
         usuarios,
